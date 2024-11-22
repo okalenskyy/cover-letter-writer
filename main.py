f3496a5ceb95fcf4e7240ad58c6cc00253ac476a
Oleg Kalenskyy <57141635+okalenskyy@users.noreply.github.com>
2024-11-22 18:53:18 +0000

    style added

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ with st.form('input_form'):
     role = st.text_input('Job title/role')
     referral = st.text_input('Source of information')
     size = st.number_input('Size of Cover Letter')
-    size = st.selectbox('Cover Letter style', ['Conversational','Persuasive','Non-dramatic','in StoryBrand Framework'])
+    style = st.selectbox('Cover Letter style', ['Conversational','Persuasive','Non-dramatic','in StoryBrand Framework'])
     
     submitted = st.form_submit_button("Generate Cover Letter")
 
